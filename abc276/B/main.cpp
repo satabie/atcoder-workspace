34e750e443c26aeb1500167a3ab22224191a1ce8
satabie <0426193@gmail.com>
2022-11-06 19:54:44 +0900

    solved abc276

--- abc276/B/main.cpp
+++ abc276/B/main.cpp
@@ -1,12 +1,29 @@
-#define _GLIBCXX_DEBUG
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define all(v) v.begin(), v.end()
 using vi = vector<int>;
 using vii = vector<vi>;
 
-int main()
-{
-		return 0;
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vii ans(n + 1);
+
+  for (int i = 0; i < m; i++) {
+    int a, b;
+    cin >> a >> b;
+    ans[a].push_back(b);
+    ans[b].push_back(a);
+  }
+
+  for (int i = 1; i <= n; i++) {
+    sort(all(ans[i]));
+    cout << ans[i].size();
+    for (int a : ans[i]) {
+      cout << " " << a;
+    }
+    cout << endl;
+  }
+  return 0;
 }
--- abc276/C/main.cpp
+++ abc276/C/main.cpp
@@ -1,12 +1,30 @@
 #define _GLIBCXX_DEBUG
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define all(v) v.begin(), v.end()
 using vi = vector<int>;
 using vii = vector<vi>;
 
-int main()
-{
-		return 0;
+int main() {
+  int n;
+  cin >> n;
+  vi p(n);
+  rep(i, n) cin >> p[i];
+
+  int j = n - 2;
+  while (p[j] < p[j + 1]) { // p_j > p_j+1になるまで
+    j--;
+  }
+
+  int k = n - 1;
+  while (p[j] < p[k]) { // p_j> p_k になるまで
+    k--;
+  }
+  swap(p[j], p[k]);
+  reverse(p.begin() + j + 1, p.end());
+
+  rep(i, n) { cout << p[i] << " \n"[i + 1 == n]; }
+
+  return 0;
 }
